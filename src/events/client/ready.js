24d2b111ed4a68d672abe6a544439f13f0623b48
Raxxyy <42278360+Raxxyy@users.noreply.github.com>
2019-11-10 22:39:55 +0100

    Update ready.js

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,6 +1,38 @@
-module.exports = async (bot) => {
+module.exports = async (bot, message) => {
 
     console.log(`${bot.user.username} is currently online!\n`);
+    
+    Pres = [
+        zimbabwe = {
+            status: 'idle',
+            game: {
+                name: 'Overtaking Zimbabwe',
+                type: 'STREAMING',
+                url: 'http://twitch.tv/nigger'
+            }
+        },
+        
+        torture = {
+            status: 'idle',
+            game: {
+                name: 'Nigger Disposal Simulator',
+                type: 'STREAMING',
+                url: 'http://twitch.tv/nigger'
+            }
+        },
+        
+        cancer = {
+            status: 'idle',
+            game: {
+                name: 'Obama you msut die',
+                type: 'STREAMING',
+                url: 'http://twitch.tv/nigger'
+            }
+        }
+    ]
 
-    bot.user.setActivity('with toys', { type: 'PLAYING' } );
+    setInterval(() => {
+        const activity = Math.floor(Math.random() * (Pres.length - 1) + 1);
+        bot.user.setPresence(Pres[activity]);
+    }, 10000);   
 } 
