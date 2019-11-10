525f0f030675b36c54b6adf8be54e49dfc63ba80
Raxxyy <42278360+Raxxyy@users.noreply.github.com>
2019-11-10 22:38:53 +0100

    Update Utils.js

--- src/Utils.js
+++ src/Utils.js
@@ -1,6 +1,5 @@
 const { RichEmbed } = require('discord.js');
 
-
 module.exports.error = {
     noPerms: (message, perm) => {  
         const embed = new RichEmbed();
@@ -10,12 +9,44 @@ module.exports.error = {
     
         message.channel.send(embed).then(m => m.delete(6000));
     },
+
     notOwner: (message, ops) => {
+        const embed = new RichEmbed();
+        embed.setAuthor(message.author.username, message.author.displayAvatarURL);
+        embed.setTitle('ERROR: NOT BOT OWMER');
+        embed.addField('Not my bot owner', ops.OwnerTag);
 
+        message.channel.send(embed).then(m => m.delete(6000));
+    },
+
+    errorMessage: (message) => {
+        message.channel.send('Oopsie, an error happened!');
+    },
+
+    noSpotify: (message) => {
+        message.channel.send('This user isn\'t listening to Spotify!');
     }
-
 }
 
-module.exports.warning = {
+module.exports.other = {
+    getMember: function(message, toFind = '') {
+        toFind = toFind.toLowerCase();
+
+        let target = message.guild.members.get(toFind);
     
+        if (!target && message.mentions.members)
+            target = message.mentions.members.first();
+
+        if (!target && toFind) {
+            target = message.guild.members.find(member => {
+                return member.displayName.toLowerCase().includes(toFind) ||
+                member.user.tag.toLowerCase().includes(toFind)
+            });
+        }
+        
+        if (!target) 
+            target = message.member;
+        
+        return target;
+    }
 }
