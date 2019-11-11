d68c8e1565d0529b392f699660b6ef218c0f5d16
Raxxyy <42278360+Raxxyy@users.noreply.github.com>
2019-11-11 22:01:38 +0100

    Update SetAvatar.js

--- src/commands/image/SetAvatar.js
+++ src/commands/image/SetAvatar.js
@@ -8,6 +8,15 @@ module.exports = {
     },
     run: async (bot, message, args, ops) => {
 
+        IDs = [
+            ops.OwnerID,
+            '257243318969303041',
+            '358516466267586560',
+            '421811254185689089',
+            '200684310901030912',
+            '307526296852758529'
+        ];
+
         try {
 
             if (talkedRecently.has(message.author.id)) {
@@ -16,12 +25,11 @@ module.exports = {
                 
             } else {
     
-                if (message.author.id != ops.OwnerID) return error.notOwner(message, ops.OwnerTag);
+                if (message.author.id != IDs[0 || 1 || 2 || 3 || 4 || 5]) return error.notRightID(message);
     
                 let avatarURL = args[0];
-    
-                bot.user.setAvatar(`${avatarURL}`)
-                .then(message.channel.send('Done!'));
+
+                bot.user.setAvatar(`${avatarURL}`).then(message.channel.send('Done!'));
     
                 talkedRecently.add(message.author.id);
     
